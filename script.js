3ebb422b802df1577224e1a67c69e870e02b4607
Kyle E. Mitchell <kyle@kemitchell.com>
2020-03-14 12:31:05 -0700

    Add notes to credit parts

--- script.js
+++ script.js
@@ -234,7 +234,8 @@ var parts = [
     text: [
       'You must give this software and each contributor credit for contribution to other work, be they goods or services, that you produce or provide using this software.',
       'In general, you must give credit in such a way that the audience for your work can freely and readily find a written notice identifying this software, by name, as a contribution to your work, as well as each contributor, by name, as a contributor to this software.  You must not do anything to stop the audience for your work from sharing, publishing, or using credits.'
-    ].join(SENTENCE_SEPARATOR)
+    ].join(SENTENCE_SEPARATOR),
+    note: 'Require credit for work.'
   },
   {
     heading: 'Credit Conventions',
@@ -247,7 +248,8 @@ var parts = [
       ', according to ',
       { url: 'https://creditstxt.com' },
       '.'
-    ]
+    ],
+    note: 'Require credits in the conventional way for each medium.'
   },
   {
     heading: 'Right to Decline Credit',
@@ -255,7 +257,8 @@ var parts = [
     text: [
       'On written request from a contributor, you must remove their name from any credits you make available for work they do not want to be associated with going forward.',
       'On written request from all contributors to this software, you must do the same for the name of this software.'
-    ].join(SENTENCE_SEPARATOR)
+    ].join(SENTENCE_SEPARATOR),
+    note: 'Allow contributors to decline credit for specific uses.'
   },
   {
     heading: 'Disclaimer',
